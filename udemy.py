7a89f822b9c7443db39200a09e2cc5a822d20314
zestymec <m.umer394aziz@gmail.com>
2026-07-11 12:26:09 +0500

    Add initial implementation of string manipulations and formatted printing

---
+++ udemy.py
@@ -0,0 +1,15 @@
+# umer = 'hi'
+
+# print(umer*3)
+
+
+# udemy = 'isumercourse'
+
+# print(udemy[1:5])
+
+# umer = "%s and %d are umer16"
+# print(umer%("umer" , 16))
+
+value = "bana"
+value2 = "na"
+print(f'this fruite is {value + value2}')
