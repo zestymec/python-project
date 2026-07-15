381c3692db2e479dde7bf99122bb96a0316cdc1a
zestymec <m.umer394aziz@gmail.com>
2026-07-15 10:40:54 +0500

    Add band name generator and improve task.py readability

---
+++ bandnamegeneratorproject.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name generator by zestymec")
+cityname = input("Whats your city name")
+petsname = input("whats you pets name")
+print("your band name is " + cityname+petsname)
--- task.py
+++ task.py
@@ -1,7 +1,12 @@
 # print("hello"+ input("What is yous name?") + "!")
 
-name = input("what is your name")
-age = input("what is your age")
-namelength = len(name)
-print(name + "is your name okay" + "!" + age + "is your age okay !" + "and your name have " + str(namelength) + "characters")
+# name = input("what is your name")
+# age = input("what is your age")
+# namelength = len(name)
+# print(name + "is your name okay" + "!" + age + "is your age okay !" + "and your name have " + str(namelength) + "characters")
 
+# print(len(input("what is your name bro ?")))
+# make you code readable
+ni = "Agdfpple"
+l = len(ni)
+print(l)
